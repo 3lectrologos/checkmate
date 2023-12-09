db793b58551d8d63401a521dc2ac4e5cc0bfb5de
Alkis Gotovos <alkis.gotovos@gmail.com>
2023-12-09 16:30:31 +0100

    Fix imports after directory restructure

--- tests/test_level5.py
+++ tests/test_level5.py
@@ -1,5 +1,5 @@
 from . import get_response
-from index import SuccessResult, SyntaxErrorResult, RuntimeErrorResult, TimeoutResult, FailResult
+from run_python_tests import SuccessResult, SyntaxErrorResult, RuntimeErrorResult, TimeoutResult, FailResult
 
 
 def test_linked_list_operations_not_available():
